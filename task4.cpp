57f10e5dfaaf4e200497b8086166b66f734972eb
Dev Spain <9984398@gmail.com>
2022-08-25 19:52:21 +0300

    Fix request

--- task4.cpp
+++ task4.cpp
@@ -1,27 +1,35 @@
 //
 // Created by Морозова Арина on 25.08.2022.
 //
+//FIXME: Replace with <cstdio>
 #include <stdio.h>
 #include <cmath>
-double getCircleArea (double radius){
-    return M_PI*pow(radius,2);
-}
-double getSquareArea(double side){
-    return pow(side,2);
 
-}
-double equalTriangle(double high){
-    return pow(high,2)/sqrt(3);
 
+double getCircleArea (double radius) {
+    return M_PI * pow (radius, 2);
 }
 
-double maxTwo(double a,double b){
-    return a>b?a:b;
+double getSquareArea (double side) {
+    return pow (side, 2);
+
 }
-int main(){
-    double square,triangle,circle;
-    scanf("%f","%f","%f",&circle,&triangle);
-    double BiggestArea=(maxTwo(maxTwo(getSquareArea(square), getCircleArea(circle)),equalTriangle(triangle)));
-    printf("%f",&BiggestArea);
+
+double equalTriangle (double high) {
+    return pow (high, 2) / sqrt (3);
+
+}
+
+double maxTwo (double a, double b) {
+    return a > b ? a : b;
+}
+
+int main () {
+    double square, triangle, circle;
+    scanf ("%lf %lf %lf", &circle, &triangle, &square);
+    //TODO: Print out figure name instead od area value
+    double BiggestArea = (maxTwo (maxTwo (getSquareArea (square), getCircleArea (circle)), equalTriangle (triangle)));
+    printf ("%f", &BiggestArea);
+    //FIXME: Pass by value not by reference
 
 }
